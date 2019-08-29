736a4014632d1638a7d6d3513607eaf52c13bb41
Zaif Senpai <escbutt@gmail.com>
2019-08-29 16:16:57 +0500

    Added PRSContextFactory class. It will be used when applying migrations from console.

---
+++ PRSClassesManagement/PRSContextFactory.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity.Infrastructure;
+
+namespace PRSClassesManagement
+{
+    class PRSContextFactory : IDbContextFactory<PRSContext>
+    {
+        public PRSContext Create()
+        {
+            return PRSContext.GetInstance();
+        }
+    }
+}
